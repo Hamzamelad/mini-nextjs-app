08667851da62f2af26fd060c018428d616d78e82
Hamzamelad <hamza99melad@gmail.com>
2025-02-05 13:13:35 +0200

    Using sqlite instead of postgresql

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -6,37 +6,17 @@ type ResponseData = {
     message: string
 }
 
+export async function GET() {
+    try {
+        const users = await db.user.findMany();
 
-
-// export async function GET(
-//     req: NextApiRequest,
-//     res: NextApiResponse<ResponseData>
-// ) {
-
-//     res.status(200).json({ message: 'Hello from Next.js!' })
-
-    // try {
-    //     await db.$connect();
-    //     return NextResponse.json({ success: '✅ Connected to the database successfully!' })
-    // } catch (error) {
-    //     return NextResponse.json({ '❌ Failed to connect': error })
-    // } finally {
-    //     await db.$disconnect();
-    // }
-
-
-    //     try {
-    //         const users = await db.user.findMany();
-
-    //         return NextResponse.json(users)
-    //     } catch (error) {
-    //         return NextResponse.json(error)
-    //     }
-
-// }
+        return Response.json(users)
+    } catch (error) {
+        return Response.json(error)
+    }
+}
 
 export async function POST(request: NextRequest) {
-
     const dummyUsers = [
         { username: 'john', email: 'john@g.com' },
         { username: 'mark', email: 'mark@g.com' },
