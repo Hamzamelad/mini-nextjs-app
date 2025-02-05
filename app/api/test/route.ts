989400cf71f44ef57f5a1125ea8ff6a39a38f0a6
Hamzamelad <hamza99melad@gmail.com>
2025-02-05 12:49:10 +0200

    Test api on production

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,14 +4,14 @@ import db from "@/lib/db";
 export async function GET() {
 
     // 2
-    try {
-        await db.$connect();
-        return Response.json({ success: '✅ Connected to the database successfully!' })
-    } catch (error) {
-        return Response.json({ '❌ Failed to connect': error })
-    } finally {
-        await db.$disconnect();
-    }
-    
+    // try {
+    //     await db.$connect();
+    //     return Response.json({ success: '✅ Connected to the database successfully!' })
+    // } catch (error) {
+    //     return Response.json({ '❌ Failed to connect': error })
+    // } finally {
+    //     await db.$disconnect();
+    // }
+
     return Response.json({ message: 'success' })
 }
